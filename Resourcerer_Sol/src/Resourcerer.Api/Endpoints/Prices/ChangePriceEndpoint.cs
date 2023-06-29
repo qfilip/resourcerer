c4030613069cc76d558d8b4da80dc7a124081e82
filip.kukolj <filip.kukolj@arcadisgen.com>
2023-06-29 14:49:27 +0200

    added ChangePrice endpoint handler

---
+++ Resourcerer_Sol/src/Resourcerer.Api/Endpoints/Prices/ChangePriceEndpoint.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Resourcerer.Dtos.Prices;
+using Resourcerer.Logic.Prices.Commands;
+
+namespace Resourcerer.Api.Endpoints.Prices;
+
+public class ChangePriceEndpoint
+{
+    public static async Task<IResult> Action(
+        [FromBody] PriceDto dto,
+        [FromServices] Pipeline pipeline,
+        [FromServices] ChangePrice.Handler handler)
+    {
+        return await pipeline.Pipe(handler, dto, nameof(ChangePrice));
+    }
+}
