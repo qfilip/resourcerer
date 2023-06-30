7aca87694ddd55a4beba1f689feec7d54dc5d7dd
phq <kukolj.filip@gmail.com>
2023-06-30 21:15:08 +0200

    updated tests with faked DB moved to Showcase.cs (will use in memory DB from now on)

--- Resourcerer_Sol/tests/Resourcerer.UnitTests/Logic/HandlerTests/Elements/Queries/GetAllElementsOverviewTests.cs
+++ Resourcerer_Sol/tests/Resourcerer.UnitTests/Logic/HandlerTests/Elements/Queries/GetAllElementsOverviewTests.cs
@@ -1,23 +1,17 @@
-﻿using FakeItEasy;
-using Microsoft.EntityFrameworkCore;
-using MockQueryable.FakeItEasy;
+﻿using Microsoft.EntityFrameworkCore;
 using Resourcerer.DataAccess.Contexts;
 using Resourcerer.DataAccess.Entities;
-using Resourcerer.Logic;
-using Resourcerer.Logic.Queries.Elements;
-using Resourcerer.UnitTests.Utilities.FluentMocks;
-using Resourcerer.UnitTests.Utilities.MemoryContext;
+using Resourcerer.UnitTests.Utilities;
 
 namespace Resourcerer.UnitTests.Logic.HandlerTests.Elements.Queries;
 
 public class GetAllElementsOverviewTests
 {
-    private readonly IAppDbContext _appDbContext;
     private readonly IAppDbContext _testDbContext;
 
     public GetAllElementsOverviewTests()
     {
-        _appDbContext = A.Fake<IAppDbContext>();
+        
         _testDbContext = new ContextCreator().GetTestDbContext();
     }
 
@@ -39,119 +33,5 @@ public class GetAllElementsOverviewTests
         Assert.True(categories.Count == 7);
     }
 
-    [Fact]
-    public async void CorrectlySumsUsageDetails_When_ElementsArePurchased()
-    {
-        var elementsMock = new List<Element>
-        {
-            FluentElementMocker
-            .Create("5th-element")
-            .AddElementPurchasedEvents(new List<(double priceByUnit, int unitsBought)>
-            {
-                (1d, 5),
-                (1d, 5)
-            })
-            .AddUnitOfMeasure("Foo", "f")
-            .Build()
-        }.AsQueryable().BuildMockDbSet();
-
-        A.CallTo(() => _appDbContext.Elements).Returns(elementsMock);
-        A.CallTo(() => _appDbContext.CompositeSoldEvents).Returns(new List<CompositeSoldEvent>().AsQueryable().BuildMockDbSet());
-
-        var handler = new GetAllElementsOverview.Handler(_appDbContext);
-        var result = await handler.Handle(new Unit());
-
-        Assert.Equal(HandlerResultStatus.Ok, result.Status);
-
-        Assert.Equal(10, result.Object![0].UnitsPurchased);
-        Assert.Equal(10d, result.Object![0].PurchaseCosts);
-        Assert.Equal(10, result.Object![0].UnitsInStock);
-    }
-
-    [Fact]
-    public async void CorrectlySumsUsageDetails_When_CompositeIsSold()
-    {
-        var compositeId = Guid.NewGuid();
-        var elementsMock = new List<Element>
-        {
-            FluentElementMocker
-            .Create("5th-element")
-            .AddUnitOfMeasure("Foo", "f")
-            .AddElementPurchasedEvents(new List<(double priceByUnit, int unitsBought)>
-            {
-                (1d, 6)
-            })
-            .AddExcerpts(new List<(Guid compositeId, double quantity)>
-            {
-                (compositeId, 3)
-            })
-            .Build()
-        }.AsQueryable().BuildMockDbSet();
-
-        A.CallTo(() => _appDbContext.Elements).Returns(elementsMock);
-        A.CallTo(() => _appDbContext.CompositeSoldEvents)
-            .Returns(new List<CompositeSoldEvent> {
-                new CompositeSoldEvent
-                {
-                    CompositeId = compositeId,
-                    PriceByUnit = 1,
-                    UnitsSold = 1
-                },
-                new CompositeSoldEvent
-                {
-                    CompositeId = compositeId,
-                    PriceByUnit = 1,
-                    UnitsSold = 1
-                },
-                new CompositeSoldEvent
-                {
-                    CompositeId = Guid.Empty,
-                    PriceByUnit = 1,
-                    UnitsSold = 1
-                }
-            }.AsQueryable().BuildMockDbSet());
-
-        var handler = new GetAllElementsOverview.Handler(_appDbContext);
-        var result = await handler.Handle(new Unit());
-
-        Assert.Equal(HandlerResultStatus.Ok, result.Status);
-
-        Assert.Equal(6, result.Object![0].UnitsPurchased);
-        Assert.Equal(6d, result.Object![0].PurchaseCosts);
-        Assert.Equal(0, result.Object![0].UnitsInStock);
-    }
-
-    [Fact]
-    public async void CorrectlySumsUsageDetails_When_ElementIsSold()
-    {
-        var elementsMock = new List<Element>
-        {
-            FluentElementMocker
-            .Create("5th-element")
-            .AddUnitOfMeasure("Foo", "f")
-            .AddElementPurchasedEvents(new List<(double priceByUnit, int unitsBought)>
-            {
-                (1d, 6)
-            })
-            .AddElementSoldEvents(new List<(double priceByUnit, int unitsSold)>
-            {
-                (1d, 3),
-                (1d, 3)
-            })
-            .Build()
-        }.AsQueryable().BuildMockDbSet();
-
-        A.CallTo(() => _appDbContext.Elements).Returns(elementsMock);
-        A.CallTo(() => _appDbContext.CompositeSoldEvents)
-            .Returns(new List<CompositeSoldEvent>().AsQueryable().BuildMockDbSet());
-
-        var handler = new GetAllElementsOverview.Handler(_appDbContext);
-        var result = await handler.Handle(new Unit());
-
-        Assert.Equal(HandlerResultStatus.Ok, result.Status);
-
-        Assert.Equal(6, result.Object![0].UnitsPurchased);
-        Assert.Equal(6d, result.Object![0].PurchaseCosts);
-        Assert.Equal(0, result.Object![0].UnitsInStock);
-    }
+    
 }
--- Resourcerer_Sol/tests/Resourcerer.UnitTests/Utilities/ContextCreator.cs
+++ Resourcerer_Sol/tests/Resourcerer.UnitTests/Utilities/ContextCreator.cs
@@ -5,33 +5,33 @@ using Resourcerer.Logic;
 using Resourcerer.Logic.Commands.Mocks;
 using Resourcerer.Logic.Queries.Mocks;
 
-namespace Resourcerer.UnitTests.Utilities.MemoryContext;
+namespace Resourcerer.UnitTests.Utilities;
 
 public class ContextCreator : IDisposable
 {
     private readonly SqliteConnection _connection;
-    private readonly DbContextOptions<TestDbContext> _options;
+    private readonly DbContextOptions<AppDbContext> _options;
     public ContextCreator()
     {
         _connection = new SqliteConnection("Filename=:memory:");
         _connection.Open();
 
-        _options = new DbContextOptionsBuilder<TestDbContext>()
+        _options = new DbContextOptionsBuilder<AppDbContext>()
             .UseSqlite(_connection)
             .Options;
 
-        var context = new TestDbContext(_options);
-        if(context.Database.EnsureCreated())
+        var context = new AppDbContext(_options);
+        if (context.Database.EnsureCreated())
         {
             SeedMockData(context);
         }
     }
     public IAppDbContext GetTestDbContext()
     {
-        return new TestDbContext(_options);
+        return new AppDbContext(_options);
     }
 
-    private void SeedMockData(TestDbContext context)
+    private void SeedMockData(AppDbContext context)
     {
         var getHandler = new GetMockedDatabaseData.Handler();
         var dbData = getHandler.Handle(new Unit()).GetAwaiter().GetResult().Object!;
@@ -39,7 +39,7 @@ public class ContextCreator : IDisposable
         var seedHandler = new SeedMockData.Handler(context);
         seedHandler.Handle(dbData).GetAwaiter().GetResult();
     }
-    
+
     public void Dispose()
     {
         _connection.Dispose();
