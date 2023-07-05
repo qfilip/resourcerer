d711c5bf2bf4a5b28debfec2a58d8ab4b61d7156
filip.kukolj <filip.kukolj@arcadisgen.com>
2023-07-05 10:21:11 +0200

    added CreateElementDeliveredEvent tests outline

---
+++ Resourcerer_Sol/src/Resourcerer.Logic/Commands/ElementEvents/CreateElementDeliveredEvent.cs
@@ -0,0 +1,57 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Resourcerer.DataAccess.Contexts;
+using Resourcerer.DataAccess.Entities;
+using Resourcerer.Dtos;
+
+namespace Resourcerer.Logic.Commands.ElementEvents;
+
+public static class CreateElementDeliveredEvent
+{
+    public class Handler : IAppHandler<CreateElementDeliveredEventDto, Unit>
+    {
+        private readonly IAppDbContext _appDbContext;
+        public Handler(IAppDbContext appDbContext)
+        {
+            _appDbContext = appDbContext;
+        }
+
+        public async Task<HandlerResult<Unit>> Handle(CreateElementDeliveredEventDto request)
+        {
+            var cancellationEvent = await _appDbContext.ElementPurchaseCancelledEvents
+                .FirstOrDefaultAsync(x => x.ElementPurchasedEventId == request.ElementPurchasedEventId);
+
+            if (cancellationEvent != null)
+            {
+                var error = "Purchase was cancelled, and cannot be delivered";
+                return HandlerResult<Unit>.ValidationError(error);
+            }
+
+            var purchaseEvent = await _appDbContext.ElementPurchasedEvents
+                .FirstOrDefaultAsync(x => x.Id == request.ElementPurchasedEventId);
+
+            if (purchaseEvent == null)
+            {
+                return HandlerResult<Unit>.NotFound("Purchase event not found");
+            }
+
+            var deliveredEvent = new ElementDeliveredEvent
+            {
+                ElementPurchasedEventId = request.ElementPurchasedEventId
+            };
+
+            var elementInstance = new Instance
+            {
+                ElementId = purchaseEvent.ElementId,
+                Quantity = purchaseEvent.UnitsBought,
+                ExpiryDate = request.InstanceExpiryDate
+            };
+
+            _appDbContext.ElementDeliveredEvents.Add(deliveredEvent);
+            _appDbContext.Instances.Add(elementInstance);
+
+            await _appDbContext.SaveChangesAsync();
+
+            return HandlerResult<Unit>.Ok(new Unit());
+        }
+    }
+}
---
+++ Resourcerer_Sol/tests/Resourcerer.UnitTests/Logic/Commands/ElementEvents/CreateElementDeliveredEventTests.cs
@@ -0,0 +1,29 @@
+﻿using Resourcerer.UnitTests.Utilities.TestDatabaseMocks;
+using Resourcerer.UnitTests.Utilities;
+using Resourcerer.DataAccess.Contexts;
+using Resourcerer.Logic.Commands.ElementEvents;
+
+namespace Resourcerer.UnitTests.Logic.Commands.ElementEvents;
+
+public class CreateElementDeliveredEventTests
+{
+    private readonly IAppDbContext _testDbContext;
+    private readonly CreateElementDeliveredEvent.Handler _handler;
+    public CreateElementDeliveredEventTests()
+    {
+        _testDbContext = new ContextCreator(CarpenterDbMocker.GetSeed).GetTestDbContext();
+        _handler = new CreateElementDeliveredEvent.Handler(_testDbContext);
+    }
+
+    [Fact]
+    public async Task Ok_When_ElementPurchasedEvent_Exists() { }
+
+    [Fact]
+    public async Task ValidationError_When_ElementPurchaseCancelledEvent_Exists() { }
+
+    [Fact]
+    public async Task IsIdempotent() { }
+
+    [Fact]
+    public async Task ThrowsException_When_ElementPurchasedEvent_DoesntExist() { }
+}
