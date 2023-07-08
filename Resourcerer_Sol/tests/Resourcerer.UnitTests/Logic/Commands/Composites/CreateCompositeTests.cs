d48347f59cfc201a84ccc5e487b28f3bfa327bf3
phq <kukolj.filip@gmail.com>
2023-07-08 13:09:57 +0200

    added CreateComposite tests outlines

---
+++ Resourcerer_Sol/tests/Resourcerer.UnitTests/Logic/Commands/Composites/CreateCompositeTests.cs
@@ -0,0 +1,36 @@
+﻿using Resourcerer.Logic.Commands.Composites;
+
+namespace Resourcerer.UnitTests.Logic.Commands.Composites;
+
+public class CreateCompositeTests : TestsBase
+{
+    private readonly CreateComposite.Handler _handler;
+    public CreateCompositeTests()
+    {
+        _handler = new CreateComposite.Handler(_testDbContext);
+    }
+
+    [Fact]
+    public void When_RequiredElements_Exists_Then_Ok()
+    {
+
+    }
+
+    [Fact]
+    public void When_RequiredElements_NotFound_Then_ValidationError()
+    {
+
+    }
+
+    [Fact]
+    public void When_ElementWithNoPriceUsed_Then_ValidationError()
+    {
+
+    }
+
+    [Fact]
+    public void When_Composite_WithSameName_Exists_Then_ValidationError()
+    {
+
+    }
+}
---
+++ Resourcerer_Sol/tests/Resourcerer.UnitTests/Logic/TestsBase.cs
@@ -0,0 +1,13 @@
+﻿using Resourcerer.DataAccess.Contexts;
+using Resourcerer.UnitTests.Utilities;
+
+namespace Resourcerer.UnitTests.Logic;
+
+public class TestsBase
+{
+    protected readonly AppDbContext _testDbContext;
+    public TestsBase()
+    {
+        _testDbContext = new ContextCreator().GetTestDbContext();
+    }
+}
