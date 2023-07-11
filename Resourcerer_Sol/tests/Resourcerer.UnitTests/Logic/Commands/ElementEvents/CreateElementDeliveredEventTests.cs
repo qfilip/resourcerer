1e35b4bafb2a2f83af5d5bec5c2ac815d650549b
phq <kukolj.filip@gmail.com>
2023-07-11 10:34:18 +0200

    updating unit tests - WIP

--- Resourcerer_Sol/tests/Resourcerer.UnitTests/Logic/Commands/ElementEvents/CreateElementDeliveredEventTests.cs
+++ Resourcerer_Sol/tests/Resourcerer.UnitTests/Logic/Commands/ElementEvents/CreateElementDeliveredEventTests.cs
@@ -1,111 +1,111 @@
-﻿//using Resourcerer.DataAccess.Contexts;
-//using Resourcerer.DataAccess.Entities;
-//using Resourcerer.Dtos;
-//using Resourcerer.Logic;
-//using Resourcerer.Logic.Commands.ElementEvents;
-//using Resourcerer.UnitTests.Utilities;
-//using Resourcerer.UnitTests.Utilities.Mocker;
+﻿using Resourcerer.DataAccess.Contexts;
+using Resourcerer.DataAccess.Entities;
+using Resourcerer.Dtos;
+using Resourcerer.Logic;
+using Resourcerer.Logic.Commands.ElementEvents;
+using Resourcerer.UnitTests.Utilities;
+using Resourcerer.UnitTests.Utilities.Mocker;
 
-//namespace Resourcerer.UnitTests.Logic.Commands.ElementEvents;
+namespace Resourcerer.UnitTests.Logic.Commands.ElementEvents;
 
-//public class CreateElementDeliveredEventTests
-//{
-//    private readonly AppDbContext _testDbContext;
-//    private readonly ElementPurchasedEvent _testPurchasedEvent;
-//    private readonly CreateElementDeliveredEvent.Handler _handler;
-//    public CreateElementDeliveredEventTests()
-//    {
-//        _testDbContext = new ContextCreator().GetTestDbContext();
-//        _testPurchasedEvent = Mocker.MockElementPurchasedEvent(_testDbContext);
-//        _handler = new CreateElementDeliveredEvent.Handler(_testDbContext);
-//    }
+public class CreateElementDeliveredEventTests
+{
+    private readonly AppDbContext _testDbContext;
+    private readonly ElementPurchasedEvent _testPurchasedEvent;
+    private readonly CreateElementDeliveredEvent.Handler _handler;
+    public CreateElementDeliveredEventTests()
+    {
+        _testDbContext = new ContextCreator().GetTestDbContext();
+        _testPurchasedEvent = Mocker.MockElementPurchasedEvent(_testDbContext);
+        _handler = new CreateElementDeliveredEvent.Handler(_testDbContext);
+    }
 
-//    [Fact]
-//    public void When_ElementPurchasedEvent_Exists_Then_Ok_And_ElementInstance_Created()
-//    {
-//        // arrange
-//        _testDbContext.ElementPurchasedEvents.Add(_testPurchasedEvent);
-//        _testDbContext.SaveChanges();
+    [Fact]
+    public void When_ElementPurchasedEvent_Exists_Then_Ok_And_ElementInstance_Created()
+    {
+        // arrange
+        _testDbContext.ElementPurchasedEvents.Add(_testPurchasedEvent);
+        _testDbContext.SaveChanges();
 
-//        var dto = new CreateElementDeliveredEventDto
-//        {
-//            ElementPurchasedEventId = _testPurchasedEvent.Id
-//        };
+        var dto = new CreateElementDeliveredEventDto
+        {
+            ElementPurchasedEventId = _testPurchasedEvent.Id
+        };
 
-//        // act
-//        var result = _handler.Handle(dto).GetAwaiter().GetResult();
-//        var deliveredEventEntities = _testDbContext.ElementDeliveredEvents.ToList();
-//        var instanceEntities = _testDbContext.Instances.ToList();
+        // act
+        var result = _handler.Handle(dto).GetAwaiter().GetResult();
+        var deliveredEventEntities = _testDbContext.ElementDeliveredEvents.ToList();
+        var instanceEntities = _testDbContext.Instances.ToList();
 
-//        // assert
-//        Assert.Equal(eHandlerResultStatus.Ok, result.Status);
-//        Assert.Single(deliveredEventEntities);
-//        Assert.Single(instanceEntities);
-//        Assert.Equal(_testPurchasedEvent.ElementId, instanceEntities[0].ElementId);
-//    }
+        // assert
+        Assert.Equal(eHandlerResultStatus.Ok, result.Status);
+        Assert.Single(deliveredEventEntities);
+        Assert.Single(instanceEntities);
+        Assert.Equal(_testPurchasedEvent.ElementId, instanceEntities[0].ElementId);
+    }
 
-//    [Fact]
-//    public void When_ElementPurchasedEvent_NotExists_Then_ValidationError()
-//    {
-//        var dto = new CreateElementDeliveredEventDto
-//        {
-//            ElementPurchasedEventId = _testPurchasedEvent.Id
-//        };
+    [Fact]
+    public void When_ElementPurchasedEvent_NotExists_Then_ValidationError()
+    {
+        var dto = new CreateElementDeliveredEventDto
+        {
+            ElementPurchasedEventId = _testPurchasedEvent.Id
+        };
 
-//        // act
-//        var result = _handler.Handle(dto).GetAwaiter().GetResult();
+        // act
+        var result = _handler.Handle(dto).GetAwaiter().GetResult();
 
-//        // assert
-//        Assert.Equal(eHandlerResultStatus.ValidationError, result.Status);
-//    }
+        // assert
+        Assert.Equal(eHandlerResultStatus.ValidationError, result.Status);
+    }
 
-//    [Fact]
-//    public void When_ElementPurchaseCancelledEvent_Exists_Then_ValidationError()
-//    {
-//        // arrange
-//        _testDbContext.ElementPurchasedEvents.Add(_testPurchasedEvent);
-//        _testDbContext.ElementPurchaseCancelledEvents.Add(new()
-//        {
-//            ElementPurchasedEventId = _testPurchasedEvent.Id
-//        });
-//        _testDbContext.SaveChanges();
+    [Fact]
+    public void When_ElementPurchaseCancelledEvent_Exists_Then_ValidationError()
+    {
+        // arrange
+        _testDbContext.ElementPurchasedEvents.Add(_testPurchasedEvent);
+        _testDbContext.ElementPurchaseCancelledEvents.Add(new()
+        {
+            ElementPurchasedEventId = _testPurchasedEvent.Id
+        });
+        _testDbContext.SaveChanges();
 
-//        var dto = new CreateElementDeliveredEventDto()
-//        {
-//            ElementPurchasedEventId = _testPurchasedEvent.Id
-//        };
+        var dto = new CreateElementDeliveredEventDto()
+        {
+            ElementPurchasedEventId = _testPurchasedEvent.Id
+        };
 
-//        // act
-//        var result = _handler.Handle(dto).GetAwaiter().GetResult();
-//        var entites = _testDbContext.ElementDeliveredEvents.ToList();
-        
-//        // assert
-//        Assert.Equal(eHandlerResultStatus.ValidationError, result.Status);
-//        Assert.Empty(entites);
-//    }
+        // act
+        var result = _handler.Handle(dto).GetAwaiter().GetResult();
+        var entites = _testDbContext.ElementDeliveredEvents.ToList();
 
-//    [Fact]
-//    public void IsIdempotent()
-//    {
-//        // arrange
-//        _testDbContext.ElementPurchasedEvents.Add(_testPurchasedEvent);
-//        _testDbContext.SaveChanges();
+        // assert
+        Assert.Equal(eHandlerResultStatus.ValidationError, result.Status);
+        Assert.Empty(entites);
+    }
 
-//        var dto = new CreateElementDeliveredEventDto
-//        {
-//            ElementPurchasedEventId = _testPurchasedEvent.Id
-//        };
+    [Fact]
+    public void IsIdempotent()
+    {
+        // arrange
+        _testDbContext.ElementPurchasedEvents.Add(_testPurchasedEvent);
+        _testDbContext.SaveChanges();
 
-//        // act
-//        var results = new[]
-//        {
-//            _handler.Handle(dto).GetAwaiter().GetResult(),
-//            _handler.Handle(dto).GetAwaiter().GetResult()
-//        };
-//        var entites = _testDbContext.ElementDeliveredEvents.ToList();
+        var dto = new CreateElementDeliveredEventDto
+        {
+            ElementPurchasedEventId = _testPurchasedEvent.Id
+        };
 
-//        // assert
-//        results.Every(x => Assert.Equal(eHandlerResultStatus.Ok, x.Status));
-//        Assert.Single(entites);
-//    }
-//}
+        // act
+        var results = new[]
+        {
+            _handler.Handle(dto).GetAwaiter().GetResult(),
+            _handler.Handle(dto).GetAwaiter().GetResult()
+        };
+        var entites = _testDbContext.ElementDeliveredEvents.ToList();
+
+        // assert
+        results.Every(x => Assert.Equal(eHandlerResultStatus.Ok, x.Status));
+        Assert.Single(entites);
+    }
+}
--- Resourcerer_Sol/tests/Resourcerer.UnitTests/Utilities/Mocker/Mocker.Events.cs
+++ Resourcerer_Sol/tests/Resourcerer.UnitTests/Utilities/Mocker/Mocker.Events.cs
@@ -5,5 +5,29 @@ namespace Resourcerer.UnitTests.Utilities.Mocker;
 
 internal static partial class Mocker
 {
-    
+    public static InstanceOrderedEvent MockInstanceOrderedEvent(AppDbContext context, Action<InstanceOrderedEvent>? modifier)
+    {
+        var entity = new InstanceOrderedEvent
+        {
+            Id = Guid.NewGuid(),
+            ExpectedDeliveryTime = DateTime.UtcNow,
+            TotalDiscountPercent = 0,
+            UnitPrice = 5,
+            UnitsOrdered = 5,
+            Instance = new Instance
+            {
+                Id = Guid.NewGuid(),
+                ExpiryDate = DateTime.UtcNow
+            }
+        };
+
+        if(modifier != null)
+            modifier(entity);
+        else
+            entity.Instance.Element = MockElement(context);
+
+        context.InstanceOrderedEvents.Add(entity);
+        
+        return entity;
+    }
 }
