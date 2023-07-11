480f78e9782b8c5f13bf6a416c1f5eae23234bf8
phq <kukolj.filip@gmail.com>
2023-07-11 21:16:40 +0200

    added CreateCompositeItem dto validation, handler, endpoint implementation

--- Resourcerer_Sol/src/Resourcerer.Dtos/Items/CreateCompositeItemDto.cs
+++ Resourcerer_Sol/src/Resourcerer.Dtos/Items/CreateCompositeItemDto.cs
@@ -4,13 +4,49 @@ namespace Resourcerer.Dtos;
 
 public class CreateCompositeItemDto : IBaseDto<CreateCompositeItemDto>
 {
+    public string? Name { get; set; }
+    public double PreparationTimeSeconds { get; set; }
+    public double? ExpirationTimeSeconds { get; set; }
+    public Guid CategoryId { get; set; }
+    public Guid UnitOfMeasureId { get; set; }
+    public double UnitPrice { get; set; }
+    public Dictionary<Guid, double>? ExcerptMap { get; set; }
     public AbstractValidator<CreateCompositeItemDto>? GetValidator() => new Validator();
 
     private class Validator : AbstractValidator<CreateCompositeItemDto>
     {
         public Validator()
         {
-            
+            RuleFor(x => x.Name)
+                .NotEmpty().WithMessage("Element name cannot be empty")
+                .Length(min: 3, max: 50).WithMessage("Element name must be between 3 and 50 characters long");
+
+            RuleFor(x => x.PreparationTimeSeconds)
+                .LessThan(0).WithMessage("PreparationTimeSeconds cannot be negative");
+
+            RuleFor(x => x.ExpirationTimeSeconds)
+                .Must(x =>
+                {
+                    if (x == null) return true;
+                    else return x < 0;
+                }).WithMessage("ExpirationTimeSeconds cannot be negative");
+
+            RuleFor(x => x.CategoryId)
+                .NotEmpty().WithMessage("Element's category cannot be empty");
+
+            RuleFor(x => x.UnitOfMeasureId)
+                .NotEmpty().WithMessage("Element's unit of measure cannot be empty");
+
+            RuleFor(x => x.UnitPrice)
+                .GreaterThan(0).WithMessage("Element's price must be greater than 0");
+
+            RuleFor(x => x.ExcerptMap)
+                .Must(x =>
+                {
+                    if (x == null) return false;
+                    else return x.All(kv => kv.Key != Guid.Empty && kv.Value >= 0);
+                })
+                .WithMessage("Excerpt map contains invalid data");
         }
     }
 }
--- Resourcerer_Sol/src/Resourcerer.Logic/Commands/Items/CreateCompositeItem.cs
+++ Resourcerer_Sol/src/Resourcerer.Logic/Commands/Items/CreateCompositeItem.cs
@@ -1,4 +1,6 @@
-﻿using Resourcerer.DataAccess.Contexts;
+﻿using Microsoft.EntityFrameworkCore;
+using Resourcerer.DataAccess.Contexts;
+using Resourcerer.DataAccess.Entities;
 using Resourcerer.Dtos;
 
 namespace Resourcerer.Logic.Commands.Items;
@@ -14,9 +16,80 @@ public static class CreateCompositeItem
             _appDbContext = appDbContext;
         }
 
-        public Task<HandlerResult<Unit>> Handle(CreateCompositeItemDto request)
+        public async Task<HandlerResult<Unit>> Handle(CreateCompositeItemDto request)
         {
-            throw new NotImplementedException();
+            var existing = await _appDbContext.Items
+                .FirstOrDefaultAsync(x => x.Name == request.Name);
+
+            if (existing != null)
+            {
+                var error = "Element with the same name already exist";
+                return HandlerResult<Unit>.ValidationError(error);
+            }
+
+            var category = await _appDbContext.Categories
+                .FirstOrDefaultAsync(x => x.Id == request.CategoryId);
+
+            if (category == null)
+            {
+                var error = "Requested category doesn't exist";
+                return HandlerResult<Unit>.ValidationError(error);
+            }
+
+            var uom = await _appDbContext.UnitsOfMeasure
+                .FirstOrDefaultAsync(x => x.Id == request.UnitOfMeasureId);
+
+            if (uom == null)
+            {
+                var error = "Requested unit of measure doesn't exist";
+                return HandlerResult<Unit>.ValidationError(error);
+            }
+
+            var requiredElementIds = request.ExcerptMap!.Select(x => x.Key).ToArray();
+            
+            var requiredItems = await _appDbContext.Items
+                .Where(x => requiredElementIds.Contains(x.Id))
+                .ToArrayAsync();
+
+            if(requiredItems.Length != requiredElementIds.Length)
+            {
+                var error = "All required elements don't exist";
+                return HandlerResult<Unit>.ValidationError(error);
+            }
+
+            var composite = new Item
+            {
+                Id = Guid.NewGuid(),
+                
+                Name = request.Name,
+                PreparationTimeSeconds = request.PreparationTimeSeconds,
+                ExpirationTimeSeconds = request.ExpirationTimeSeconds,
+                
+                CategoryId = category.Id,
+                UnitOfMeasureId = uom.Id,
+            };
+
+            var price = new Price
+            {
+                ItemId = composite.Id,
+                UnitValue = request.UnitPrice
+            };
+
+            var excerpts = request.ExcerptMap!
+                .Select(x => new Excerpt
+                {
+                    CompositeId = composite.Id,
+                    ElementId = x.Key,
+                    Quantity = x.Value
+                }).ToArray();
+
+            _appDbContext.Items.Add(composite);
+            _appDbContext.Prices.Add(price);
+            _appDbContext.Excerpts.AddRange(excerpts);
+
+            await _appDbContext.SaveChangesAsync();
+
+            return HandlerResult<Unit>.Ok(new Unit());
         }
     }
 }
