63465eea2d57abebb1859c07d76d657982c0b3fb
filip.kukolj <filip.kukolj@arcadisgen.com>
2023-07-17 15:25:19 +0200

    added GetItemStockInfo handler

--- Resourcerer_Sol/src/Resourcerer.Dtos/Items/ItemStockInfoDto.cs
+++ Resourcerer_Sol/src/Resourcerer.Dtos/Items/ItemStockInfoDto.cs
@@ -5,4 +5,7 @@ public class ItemStockInfoDto : IBaseDto
     public Guid Id { get; set; }
     public string? Name { get; set; }
     public InstanceInfoDto[]? InstancesInfo { get; set; }
+    public string[]? ItemType { get; set; }
+    public double ProductionCostAsComposite { get; set; }
+    public double SellingPrice { get; set; }
 }
--- Resourcerer_Sol/src/Resourcerer.Logic/Queries/Items/GetItemStockInfo.cs
+++ Resourcerer_Sol/src/Resourcerer.Logic/Queries/Items/GetItemStockInfo.cs
@@ -1,11 +1,13 @@
-﻿using Resourcerer.DataAccess.Contexts;
+﻿using Microsoft.EntityFrameworkCore;
+using Resourcerer.DataAccess.Contexts;
+using Resourcerer.Dtos;
 using Resourcerer.Dtos.Items;
 
 namespace Resourcerer.Logic.Queries.Items;
 
 public static class GetItemStockInfo
 {
-    public class Handler : IAppHandler<(Guid itemId, DateTime now), ItemStockInfoDto>
+    public class Handler : IAppHandler<(Guid ItemId, DateTime Now), ItemStockInfoDto>
     {
         private readonly AppDbContext _appDbContext;
 
@@ -14,9 +16,58 @@ public static class GetItemStockInfo
             _appDbContext = appDbContext;
         }
 
-        public Task<HandlerResult<ItemStockInfoDto>> Handle((Guid itemId, DateTime now) request)
+        public async Task<HandlerResult<ItemStockInfoDto>> Handle((Guid ItemId, DateTime Now) request)
         {
-            throw new NotImplementedException();
+            var item = await _appDbContext.Items
+                .Include(x => x.Category)
+                .Include(x => x.UnitOfMeasure)
+                .Include(x => x.Prices)
+                .Include(x => x.ElementExcerpts)
+                .Include(x => x.CompositeExcerpts)
+                    .ThenInclude(x => x.Element)
+                        .ThenInclude(x => x!.Prices)
+                // orders
+                .Include(x => x.Instances)
+                    .ThenInclude(x => x.InstanceBuyRequestedEvent)
+                        .ThenInclude(x => x!.InstanceRequestDeliveredEvent)
+                // cancelations
+                .Include(x => x.Instances)
+                    .ThenInclude(x => x.InstanceBuyRequestedEvent)
+                        .ThenInclude(x => x!.InstanceRequestCancelledEvent)
+                // discards
+                .Include(x => x.Instances)
+                    .ThenInclude(x => x.InstanceDiscardedEvents)
+                .FirstOrDefaultAsync(x => x.Id == request.ItemId);
+
+            if (item == null)
+            {
+                return HandlerResult<ItemStockInfoDto>.NotFound($"Item with id {request.ItemId} doesn't exist");
+            }
+
+            var instancesInfo = item.Instances
+                .Select(x => Functions.Instances.GetInstanceInfo(x, request.Now))
+                .OfType<InstanceInfoDto>()
+                .ToArray();
+
+            var isComposite = item.CompositeExcerpts.Any();
+
+            var usedInComposites = item.ElementExcerpts.Count();
+
+            var productionCostAsComposite = item.CompositeExcerpts
+                .SelectMany(x => x.Element!.Prices)
+                .Sum(x => x.UnitValue);
+
+            var result = new ItemStockInfoDto
+            {
+                Id = item.Id,
+                Name = item.Name,
+                InstancesInfo = instancesInfo,
+                ItemType = isComposite ? new[] { "Element", "Composite" } : new[] { "Element" },
+                ProductionCostAsComposite = productionCostAsComposite,
+                SellingPrice = item.Prices.Single().UnitValue
+            };
+
+            return HandlerResult<ItemStockInfoDto>.Ok(result);
         }
     }
 }
