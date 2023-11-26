b7bda9cfd4ed35e3e1f4748d7fec8da34802e33c
qfilip <kukolj.filip@gmail.com>
2023-11-26 12:31:26 +0100

    event handlers

---
+++ src/Resourcerer.Api/Services/V1_0/ItemEventHandler.cs
@@ -0,0 +1,19 @@
+﻿using System.Threading.Channels;
+
+namespace Resourcerer.Api.Services.V1_0;
+
+public class ItemEventHandler : BackgroundService
+{
+    private readonly ChannelReader<int> _reader;
+
+    public ItemEventHandler(ChannelReader<int> reader)
+    {
+        _reader = reader;
+    }
+
+    protected override Task ExecuteAsync(CancellationToken stoppingToken)
+    {
+        var tcs = new TaskCompletionSource<int>();
+        throw new NotImplementedException();
+    }
+}
