ce4d6e9852c3f9c94f8748c17f283208e31853fb
qfilip <kukolj.filip@gmail.com>
2023-11-26 12:32:08 +0100

    event handlers

--- src/Resourcerer.Api/Services/V1_0/ItemEventHandler.cs
+++ src/Resourcerer.Api/Services/V1_0/ItemEventHandler.cs
@@ -1,19 +1,36 @@
-﻿using System.Threading.Channels;
+﻿using Resourcerer.DataAccess.Contexts;
+using System.Threading.Channels;
 
 namespace Resourcerer.Api.Services.V1_0;
 
 public class ItemEventHandler : BackgroundService
 {
     private readonly ChannelReader<int> _reader;
+    private readonly IServiceProvider _serviceProvider;
 
-    public ItemEventHandler(ChannelReader<int> reader)
+    public ItemEventHandler(
+        ChannelReader<int> reader,
+        IServiceProvider serviceProvider)
     {
         _reader = reader;
+        _serviceProvider = serviceProvider;
     }
 
-    protected override Task ExecuteAsync(CancellationToken stoppingToken)
+    protected override async Task ExecuteAsync(CancellationToken stoppingToken)
     {
-        var tcs = new TaskCompletionSource<int>();
-        throw new NotImplementedException();
+        while(!_reader.Completion.IsCompleted)
+        {
+            var message = await _reader.ReadAsync();
+            using (var scope = _serviceProvider.CreateScope())
+            {
+                var database = scope.ServiceProvider.GetRequiredService<AppDbContext>();
+                if(database == null)
+                {
+                    throw new InvalidOperationException("Failed to resolve database service");
+                }
+
+
+            }
+        }
     }
 }
