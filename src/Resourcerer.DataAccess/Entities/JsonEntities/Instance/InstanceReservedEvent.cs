a52af46e1550c0fa747b57abbb1f45877530ef04
filip.kukolj <filip.kukolj@arcadisgen.com>
2024-03-15 13:35:46 +0100

    fixed CreateItemProductionEventHandler requirement computation

--- src/Resourcerer.DataAccess/Entities/JsonEntities/Instance/InstanceReservedEvent.cs
+++ src/Resourcerer.DataAccess/Entities/JsonEntities/Instance/InstanceReservedEvent.cs
@@ -4,6 +4,20 @@ namespace Resourcerer.DataAccess.Entities;
 
 public class InstanceReservedEvent : JsonEntityBase
 {
+    public Guid ProductionOrderId { get; set; }
     public double Quantity { get; set; }
     public string? Reason { get; set; }
+
+    public InstanceReserveCancelledEvent? CancelledEvent { get; set; }
+    public InstanceReserveUsedEvent? UsedEvent { get; set; }
+}
+
+public class InstanceReserveCancelledEvent : JsonEntityBase
+{
+    public string? Reason { get; set; }
+}
+
+public class InstanceReserveUsedEvent : JsonEntityBase
+{
+
 }
--- src/Resourcerer.Logic/V1_0/Commands/Items/Events/Production/CreateItemProductionOrder.cs
+++ src/Resourcerer.Logic/V1_0/Commands/Items/Events/Production/CreateItemProductionOrder.cs
@@ -2,9 +2,9 @@
 using Microsoft.EntityFrameworkCore;
 using Resourcerer.DataAccess.Contexts;
 using Resourcerer.DataAccess.Entities;
+using Resourcerer.DataAccess.Entities.JsonEntities;
 using Resourcerer.Dtos;
 using Resourcerer.Logic.V1_0.Functions;
-using System;
 
 namespace Resourcerer.Logic.V1_0.Commands.Items;
 public static class CreateItemProductionOrder
@@ -28,88 +28,87 @@ public static class CreateItemProductionOrder
                 return HandlerResult<Unit>.Rejected("Item not found");
             }
 
-            if(request.InstanceToUseIds.Length > 0)
+            var productionOrder = new ItemProductionOrder
+            {
+                Id = Guid.NewGuid(),
+                ItemId = item.Id,
+                Quantity = request.Quantity
+            };
+
+            if (request.InstanceToUseIds.Length > 0)
             {
                 var excerpts = await _dbContext.Excerpts
                 .Where(x => x.CompositeId == request.ItemId)
                     .Include(x => x.Element)
                         .ThenInclude(x => x!.Instances)
+                .AsNoTracking()
                 .ToArrayAsync();
 
-                var instances = excerpts
-                    .SelectMany(x => x.Element!.Instances)
-                    .ToArray();
+                //var instances = excerpts
+                //    .SelectMany(x => x.Element!.Instances)
+                //    .ToArray();
 
-                var usableInstances = RequestedInstancesExist(excerpts, instances, request.InstanceToUseIds);
-                if (usableInstances.Length == 0)
+                var availableInstances = excerpts
+                    .SelectMany(x => x.Element!.Instances, (e, x) =>
+                    {
+                        var available = Instances.GetAvailableUnitsInStock(x);
+                        if (available < e.Quantity * request.Quantity)
+                        {
+                            return null;
+                        }
+                        else
+                        {
+                            return new
+                            {
+                                x.Id,
+                                AvailableQuantity = Instances.GetAvailableUnitsInStock(x),
+                                RequiredQuantity = e.Quantity * request.Quantity
+                            };
+                        }
+                    })
+                    .Where(x => x != null)
+                    .ToList();
+
+                // all instances exist
+                var availableInstanceIds = availableInstances.Select(x => x!.Id).ToList();
+                if(!request.InstanceToUseIds.All(id => availableInstanceIds.Contains(id)))
                 {
                     return HandlerResult<Unit>.Rejected("Not all requested instances found");
                 }
 
-                PopulateRequiredQuantity(usableInstances, excerpts);
-                // TODO
+                var instancesToUpdate = await _dbContext.Instances
+                    .Where(x => availableInstanceIds.Contains(x.Id))
+                    .ToArrayAsync();
 
-                // add ReservedEvents to Instance as json property
-                // update ReservedEvents for every usable instance
+                foreach (var instance in instancesToUpdate)
+                {
+                    var requiredQuantity = availableInstances
+                        .First(x => x!.Id == instance.Id)!
+                        .RequiredQuantity;
 
-                // add UsedEvents to Instance.ReservedEvent
+                    var reservedEvent = JsonEntityBase.CreateEntity(() =>
+                        new InstanceReservedEvent
+                        {
+                            ProductionOrderId = productionOrder.Id,
+                            Quantity = requiredQuantity,
+                            Reason = $"Production of item: {item.Id}-{item.Name}"
+                        });
+                    
+                    instance.ReservedEvents.Add(reservedEvent);
+                }
             }
 
-            var entity = new ItemProductionOrder
-            {
-                ItemId = item.Id,
-                Quantity = request.Quantity
-            };
-
-            _dbContext.ItemProductionOrders.Add(entity);
+            _dbContext.ItemProductionOrders.Add(productionOrder);
+            
             await _dbContext.SaveChangesAsync();
 
             return HandlerResult<Unit>.Ok(Unit.New);
-        }
-
-        private static RequestedInstance[] RequestedInstancesExist(Excerpt[] excerpts, Instance[] instances, Guid[] requestedInstanceIds)
-        {
-            if(!instances.All(x => requestedInstanceIds.Contains(x.Id)))
-            {
-                return Array.Empty<RequestedInstance>();
-            }
-
-            var usableInstances = instances
-                .Select(x => new RequestedInstance
-                {
-                    Id = x.Id,
-                    ItemId = x.ItemId,
-                    AvailableQuantity = Instances.GetAvailableUnitsInStock(x)
-                })
-                .ToArray();
-
-            return usableInstances.All(x => requestedInstanceIds.Contains(x.Id)) ?
-                usableInstances : Array.Empty<RequestedInstance>();
-        }
-
-        private static void PopulateRequiredQuantity(RequestedInstance[] usableInstances, Excerpt[] excerpts)
-        {
-            foreach (var ui in usableInstances)
-            {
-                var usableInstance = usableInstances.First(x => x.Id == ui.Id);
-                var excerpt = excerpts.First(x => x.ElementId == usableInstance.ItemId);
-
-                usableInstance.RequiredQuantity = excerpt.Quantity;
-            }
         }
 
         public ValidationResult Validate(CreateItemProductionOrderRequestDto request)
         {
             throw new NotImplementedException();
         }
-    }
-    
-    private class RequestedInstance
-    {
-        public Guid Id { get; set; }
-        public Guid? ItemId { get; set; }
-        public double AvailableQuantity { get; set; }
-        public double RequiredQuantity { get; set; }
     }
 }
 
