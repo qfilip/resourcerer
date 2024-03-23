efac9fc48d5eee2437ac862e155ddc93b0c70bd4
qfilip <kukolj.filip@gmail.com>
2024-03-23 12:56:21 +0100

    added new data faking mechanism

--- tests/Resourcerer.UnitTests/Logic/TestsBase.cs
+++ tests/Resourcerer.UnitTests/Logic/TestsBase.cs
@@ -1,17 +1,8 @@
 ﻿using FakeItEasy;
-using Microsoft.AspNetCore.Routing;
-using Microsoft.EntityFrameworkCore;
-using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
-using Resourcerer.Api;
-using Resourcerer.Api.Endpoints;
-using Resourcerer.DataAccess.Contexts;
 using Resourcerer.DataAccess.Entities;
-using Resourcerer.DataAccess.Entities.JsonEntities;
 using Resourcerer.UnitTests.Utilities;
 using Resourcerer.UnitTests.Utilities.Faker;
-using System.Linq.Expressions;
-using QU = Resourcerer.DataAccess.Utilities.Query.Instances;
 
 namespace Resourcerer.UnitTests.Logic;
 
@@ -22,6 +13,15 @@ public class TestsBase
     public TestsBase()
     {
         _ctx = new ContextCreator().GetTestDbContext();
+    }
+
+    [Fact]
+    public void Scratchpad()
+    {
+        var instance = DF.Fake<Instance>(_ctx);
+        _ctx.SaveChanges();
+
+        _ = 0;
     }
 
     //[Fact]
@@ -58,106 +58,6 @@ public class TestsBase
     //    }
 
     //    app.StopAsync().Wait();
-    //}
-
-    //[Fact]
-    //public void QueryInspection()
-    //{
-    //    var order = new ItemProductionOrder();
-
-    //    var exp1 = QU.Expand(x => new Instance
-    //    {
-    //        Id = x.Id,
-    //        ReservedEventsJson = x.ReservedEventsJson
-    //    });
-    //    Expression<Func<Instance, Instance>> exp2 = x => new Instance { Id = x.Id, };
-
-    //    var s1 = exp1.ToString();
-    //    var s2 = exp2.ToString();
-
-    //    var q1 = _testDbContext.Instances
-    //        .Where(x => order.InstancesUsedIds.Contains(x.Id))
-    //        .Select(exp1)
-    //        .ToQueryString();
-
-    //    var q2 = _testDbContext.Instances
-    //        .Where(x => order.InstancesUsedIds.Contains(x.Id))
-    //        .Select(QU.Expand(x => new Instance
-    //        {
-    //            Id = x.Id,
-    //            ReservedEvents = x.ReservedEvents
-    //        }))
-    //        .ToQueryString();
-
-    //    var q3 = _testDbContext.Instances
-    //        .Where(x => order.InstancesUsedIds.Contains(x.Id))
-    //        .Select(exp2)
-    //        .ToQueryString();
-
-    //    var _ = 0;
-    //}
-
-    //[Fact]
-    //public void ChangeTracking()
-    //{
-    //    // https://learn.microsoft.com/en-us/ef/core/querying/tracking
-    //    DF.FakeInstance(_testDbContext);
-    //    _testDbContext.SaveChanges();
-
-    //    var i = _testDbContext.Instances
-    //        .Select(QU.Expand(x => new Instance
-    //        {
-    //            ReservedEventsJson = x.ReservedEventsJson
-    //        }))
-    //        .First();
-
-    //    _testDbContext.Attach(i);
-    //    i.Quantity = 7;
-    //    var arr = _testDbContext.ChangeTracker.Entries().ToArray();
-    //    _testDbContext.SaveChanges();
-
-    //    var v = _testDbContext.Instances.First();
-
-    //    _ = 0;
-    //}
-
-    //[Fact]
-    //public void ChangeTrackingV2()
-    //{
-    //    // https://learn.microsoft.com/en-us/ef/core/querying/tracking
-    //    var o = DF.FakeItemProductionOrder(_testDbContext, x =>
-    //    {
-    //        x.Quantity = 1;
-    //        x.StartedEvent = JsonEntityBase.CreateEntity(() => new ItemProductionStartedEvent());
-    //    });
-    //    _testDbContext.SaveChanges();
-
-    //    var o2 = _testDbContext.ItemProductionOrders
-    //        .Select(x => new ItemProductionOrder
-    //        {
-    //            Id = x.Id,
-    //            ItemId = x.ItemId,
-    //            Item = new Item { CategoryId = x.Item!.CategoryId },
-    //            StartedEventJson = x.StartedEventJson,
-    //            FinishedEventJson = x.FinishedEventJson
-    //        })
-    //        .AsNoTracking()
-    //        .First(x => x.Id == o.Id);
-
-    //    o2.FinishedEvent = JsonEntityBase.CreateEntity(() => new ItemProductionFinishedEvent());
-
-    //    _testDbContext.ItemProductionOrders.Attach(o2);
-        
-    //    _testDbContext.Entry(o2).Property(x => x.FinishedEventJson).IsModified = true;
-    //    var r = _testDbContext.ItemProductionOrders.Entry(o2).Property(x => x.ItemId).IsModified; // bugged
-    //    var r2 = _testDbContext.Entry(o2).Property(x => x.CanceledEventJson).IsModified;
-    //    _testDbContext.SaveChanges();
-
-    //    var o3 = _testDbContext.ItemProductionOrders
-    //        .AsNoTracking()
-    //        .First(x => x.Id == o.Id && x.Quantity == 1);
-
-    //    _ = 0;
     //}
 
     protected ILogger<T> MockLogger<T>() => A.Fake<ILogger<T>>();
---
+++ tests/Resourcerer.UnitTests/Utilities/Faker/DF.Functions.cs
@@ -0,0 +1,119 @@
+﻿using Resourcerer.DataAccess.Entities;
+
+namespace Resourcerer.UnitTests.Utilities.Faker;
+
+internal static partial class DF
+{
+    internal static IDictionary<Type, (Delegate, Type[])> FakingFunctions = LoadFakingFunctions();
+
+    internal static IDictionary<Type, (Delegate, Type[])> LoadFakingFunctions()
+    {
+        return new Dictionary<Type, (Delegate, Type[])>()
+        {
+            {
+                typeof(Company),
+                (() => MakeEntity(() => new Company { Name = MakeName() }),
+                new Type[0])
+            },
+            {
+                typeof(AppUser),
+                (() => MakeEntity(() => new AppUser { Name = MakeName() }),
+                [typeof(Company)])
+            },
+            {
+                typeof(Category),
+                (() => MakeEntity(() => new Category { Name = MakeName() }),
+                [typeof(Company)])
+            },
+            {
+                typeof(UnitOfMeasure),
+                (() => MakeEntity(() => new UnitOfMeasure
+                {
+                    Name = MakeName(),
+                    Symbol = "uom"
+                }),
+                [typeof(Company)])
+            },
+            {
+                typeof(Price),
+                (() => MakeEntity(() => new Price { UnitValue = 1 }),
+                [typeof(Item)])
+            },
+            {
+                typeof(Item),
+                (() => MakeEntity(() => new Item
+                {
+                    Name = MakeName(),
+                    ExpirationTimeSeconds = 1200,
+                    ProductionTimeSeconds = 10,
+                }),
+                [typeof(Category), typeof(UnitOfMeasure)])
+            },
+            {
+                typeof(ItemProductionOrder),
+                (() => MakeEntity(() => new ItemProductionOrder { Quantity = 1 }),
+                [typeof(Item)])
+            },
+            {
+                typeof(Instance),
+                (() => MakeEntity(() => new Instance { Quantity = 1 }),
+                [typeof(Company), typeof(Item)])
+            }
+        };
+    }
+
+    internal static T Fake<T>(TestDbContext ctx, Action<T>? modifier = null) where T : AppDbEntity
+    {
+        var entity = Fake(typeof(T)) as T;
+        modifier?.Invoke(entity!);
+
+        var dbSet = ctx.Set<T>();
+        dbSet.Add(entity!);
+
+        return entity!;
+    }
+
+    internal static object Fake(Type t)
+    {
+        var hasKey = FakingFunctions.TryGetValue(t, out var tuple);
+        if (!hasKey)
+        {
+            throw new Exception($"Faking function for type {t}, not found");
+        }
+
+        var fakingFunction = tuple.Item1;
+        var relationalTypes = tuple.Item2;
+
+        var entity = fakingFunction.DynamicInvoke();
+        
+        if(entity == null)
+        {
+            throw new Exception($"Faking function for type {t} returns null");
+        }
+
+        var entityType = entity.GetType();
+        if(entityType != t)
+        {
+            throw new Exception($"Faking function for type {t} returns {entityType}");
+        }
+
+        foreach (var relationalType in relationalTypes)
+        {
+            var relationalProperty = t.GetProperties()
+                .FirstOrDefault(x => x.PropertyType == relationalType);
+
+            if(relationalProperty == null)
+            {
+                throw new Exception($"Invalid mapping of relational property array. Relational type {relationalType} doesn't exist on entity type {t}");
+            }
+
+            var relationPropertyValue = relationalProperty.GetValue(entity);
+            if(relationPropertyValue == null)
+            {
+                relationalProperty.SetValue(entity, Fake(relationalProperty.PropertyType));
+            }
+        }
+
+        return entity;
+    }
+}
