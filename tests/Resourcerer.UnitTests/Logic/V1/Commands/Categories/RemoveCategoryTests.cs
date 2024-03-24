4e6e0b7cee250d628d87e210fca68ff3e9a47bf5
qfilip <kukolj.filip@gmail.com>
2024-03-24 11:18:33 +0100

    updating tests

---
+++ tests/Resourcerer.UnitTests/Logic/V1/Commands/Categories/RemoveCategoryTests.cs
@@ -0,0 +1,48 @@
+﻿using Resourcerer.DataAccess.Entities;
+using Resourcerer.Dtos.Entity;
+using Resourcerer.Logic;
+using Resourcerer.Logic.V1.Commands;
+using Resourcerer.UnitTests.Utilities;
+using Resourcerer.UnitTests.Utilities.Faker;
+
+namespace Resourcerer.UnitTests.Logic.V1.Commands.Categories;
+
+public class RemoveCategoryTests : TestsBase
+{
+    private readonly RemoveCategory.Handler _sut;
+    public RemoveCategoryTests()
+    {
+        _sut = new RemoveCategory.Handler(base._ctx);
+    }
+
+    [Fact]
+    public void HappyPath__Ok()
+    {
+        // arrange
+        var category = DF.Fake<Category>(_ctx);
+        var dto = new CategoryDto { Id = category.Id };
+        
+        _ctx.SaveChanges();
+
+        // act
+        var result = _sut.Handle(dto).Await();
+
+        // assert
+        Assert.Multiple(
+            () => Assert.Equal(eHandlerResultStatus.Ok, result.Status),
+            () =>
+            {
+                _ctx.Clear();
+                Assert.Equal(0, _ctx.Categories.Count());
+            }
+        );
+    }
+
+    [Fact]
+    public void Validator__Ok()
+    {
+        var dto = new CategoryDto { Id = Guid.Empty };
+        var result = _sut.Handle(dto).Await();
+        Assert.Single(result.Errors);
+    }
+}
--- tests/Resourcerer.UnitTests/Logic/V1/Commands/Items/ChangeItemPriceTests.cs
+++ tests/Resourcerer.UnitTests/Logic/V1/Commands/Items/ChangeItemPriceTests.cs
@@ -1,12 +1,14 @@
 ﻿using FakeItEasy;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Logging;
+using Resourcerer.DataAccess.Entities;
 using Resourcerer.DataAccess.Enums;
 using Resourcerer.Dtos.V1;
 using Resourcerer.Logic;
 using Resourcerer.Logic.V1.Commands;
 using Resourcerer.UnitTests.Utilities;
 using Resourcerer.UnitTests.Utilities.Faker;
+using System.Diagnostics;
 
 namespace Resourcerer.UnitTests.Logic.V1.Commands.Items;
 
@@ -21,19 +23,25 @@ public class ChangeItemPriceTests : TestsBase
     }
 
     [Fact]
-    public void When_AllOk_Then_NewPriceAdded_Ok()
+    public void HappyPath__Ok()
     {
         // arrange
-        var item = DF.FakeItem(_ctx);
-        _ctx.SaveChanges();
-        
-        var oldPrices = item.Prices.Select(x => x).ToList();
+        var item = DF.Fake<Item>(_ctx);
+        var price1 = DF.Fake<Price>(_ctx, x => x.Item = item);
+        var price2 = DF.Fake<Price>(_ctx, x =>
+        {
+            x.Item = item;
+            x.EntityStatus = eEntityStatus.Deleted;
+        });
+
         var dto = new V1ChangePrice
         {
             ItemId = item.Id,
             UnitPrice = 20
         };
 
+        _ctx.SaveChanges();
+        
         // act
         var result = _handler.Handle(dto).Await();
         var newPrices = _ctx.Prices
@@ -42,13 +50,25 @@ public class ChangeItemPriceTests : TestsBase
             .ToList();
 
         // assert
-        Assert.Equal(eHandlerResultStatus.Ok, result.Status);
-        Assert.Equal(oldPrices.Count + 1, newPrices.Count);
-        Assert.Equal(1, newPrices.Count(x => x.EntityStatus == eEntityStatus.Active));
+        Assert.Multiple(
+            () => Assert.Equal(eHandlerResultStatus.Ok, result.Status),
+            () =>
+            {
+                _ctx.Clear();
+                
+                var prices = _ctx.Prices
+                    .Select(x => new { x.EntityStatus, x.UnitValue })
+                    .IgnoreQueryFilters()
+                    .ToArray();
+
+                Assert.Single(prices.Where(x => x.EntityStatus == eEntityStatus.Active));
+                Assert.Single(prices.Where(x => x.UnitValue == dto.UnitPrice));
+            }
+        );
     }
 
     [Fact]
-    public void When_Element_NotExist_Then_ValidationError()
+    public void Element_NotExist_Then__NotFound()
     {
         // arrange
         var dto = new V1ChangePrice
@@ -62,32 +82,44 @@ public class ChangeItemPriceTests : TestsBase
         var result = _handler.Handle(dto).Await();
 
         // assert
-        Assert.Equal(eHandlerResultStatus.Rejected, result.Status);
+        Assert.Equal(eHandlerResultStatus.NotFound, result.Status);
     }
 
     [Fact]
-    public void When_Element_HasCorruptedPrices_Then_PricesAreFixed_Ok()
+    public void Element_HasCorruptedPrices_Then_PricesAreFixed__Ok()
     {
         // arrange
-        var item = DF.FakeItem(_ctx, null, 1, 3, true);
-        var oldPrices = item.Prices.Select(x => x).ToList();
+        var item = DF.Fake<Item>(_ctx);
+        var oldPrices = Enumerable.Range(0, 3)
+            .Select(_ => DF.Fake<Price>(_ctx, x => x.Item = item))
+            .ToArray();
+        
         var dto = new V1ChangePrice
         {
             ItemId = item.Id,
             UnitPrice = 20
         };
+        
         _ctx.SaveChanges();
 
         // act
         var result = _handler.Handle(dto).Await();
-        var newPrices = _ctx.Prices
-            .Where(x => x.ItemId == item.Id)
-            .IgnoreQueryFilters()
-            .ToList();
 
         // assert
-        Assert.Equal(eHandlerResultStatus.Ok, result.Status);
-        Assert.Equal(oldPrices.Count + 1, newPrices.Count);
-        Assert.Equal(1, newPrices.Count(x => x.EntityStatus == eEntityStatus.Active));
+        Assert.Multiple(
+            () => Assert.Equal(eHandlerResultStatus.Ok, result.Status),
+            () =>
+            {
+                _ctx.Clear();
+                
+                var dbOldPrices = _ctx.Prices
+                    .Where(x => x.ItemId == item.Id)
+                    .IgnoreQueryFilters()
+                .ToArray();
+
+                Assert.Equal(oldPrices.Length + 1, dbOldPrices.Length);
+                Assert.Single(dbOldPrices.Where(x => x.EntityStatus == eEntityStatus.Active));
+            }
+        );
     }
 }
