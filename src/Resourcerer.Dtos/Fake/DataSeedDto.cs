c62b59f4f7891cfe2435002f4ff0c90552b13c31
qfilip <kukolj.filip@gmail.com>
2024-04-05 00:24:55 +0200

    faking data endpoint handler

--- src/Resourcerer.Dtos/Fake/DataSeedDto.cs
+++ src/Resourcerer.Dtos/Fake/DataSeedDto.cs
@@ -4,18 +4,16 @@ namespace Resourcerer.Dtos.Fake;
 
 public class DataSeedDto : IDto
 {
-    public List<AppUser> AppUsers { get; set; } = new();
-    public List<Company> Companies { get; set; } = new();
-    public List<Category> Categories { get; set; } = new();
-    public List<Excerpt> Excerpts { get; set; } = new();
-    public List<UnitOfMeasure> UnitsOfMeasure { get; set; } = new();
-    public List<Price> Prices { get; set; } = new();
-    // items = new();
-    public List<Item> Items { get; set; } = new();
-    public List<ItemProductionOrder> ItemProductionOrders { get; set; } = new();
-    // instances = new();
-    public List<Instance> Instances { get; set; } = new();
-    public List<InstanceOrderedEvent> InstanceOrderedEvents { get; set; } = new();
-    public List<InstanceReservedEvent> InstanceReservedEvents { get; set; } = new();
-    public List<InstanceDiscardedEvent> InstanceDiscardedEvents { get; set; } = new();
+    public AppUser[] AppUsers { get; set; } = Array.Empty<AppUser>();
+    public Company[] Companies { get; set; } = Array.Empty<Company>();
+    public Category[] Categories { get; set; } = Array.Empty<Category>();
+    public Excerpt[] Excerpts { get; set; } = Array.Empty<Excerpt>();
+    public UnitOfMeasure[] UnitsOfMeasure { get; set; } = Array.Empty<UnitOfMeasure>();
+    public Price[] Prices { get; set; } = Array.Empty<Price>();
+    public Item[] Items { get; set; } = Array.Empty<Item>();
+    public ItemProductionOrder[] ItemProductionOrders { get; set; } = Array.Empty<ItemProductionOrder>();
+    public Instance[] Instances { get; set; } = Array.Empty<Instance>();
+    public InstanceOrderedEvent[] InstanceOrderedEvents { get; set; } = Array.Empty<InstanceOrderedEvent>();
+    public InstanceReservedEvent[] InstanceReservedEvents { get; set; } = Array.Empty<InstanceReservedEvent>();
+    public InstanceDiscardedEvent[] InstanceDiscardedEvents { get; set; } = Array.Empty<InstanceDiscardedEvent>();
 }
--- src/Resourcerer.Logic/Fake/Seed.cs
+++ src/Resourcerer.Logic/Fake/Seed.cs
@@ -1,12 +1,15 @@
 ﻿using FluentValidation.Results;
+using Microsoft.EntityFrameworkCore;
 using Resourcerer.DataAccess.Contexts;
+using Resourcerer.DataAccess.Utilities.Faking;
 using Resourcerer.Dtos.Fake;
+using System.Text.Json;
 
 namespace Resourcerer.Logic.Fake;
 
 public static class Seed
 {
-    public class Handler : IAppHandler<DataSeedDto, Unit>
+    public class Handler : IAppHandler<Unit, DataSeedDto>
     {
         private readonly AppDbContext _dbContext;
 
@@ -15,32 +18,31 @@ public static class Seed
             _dbContext = dbContext;
         }
 
-        public async Task<HandlerResult<Unit>> Handle(DataSeedDto request)
+        public async Task<HandlerResult<DataSeedDto>> Handle(Unit _)
         {
-            _dbContext.Companies.AddRange(request.Companies);
-            _dbContext.SaveChanges();
-            
-            _dbContext.AppUsers.AddRange(request.AppUsers);
-            _dbContext.Categories.AddRange(request.Categories);
-            _dbContext.UnitsOfMeasure.AddRange(request.UnitsOfMeasure);
-            _dbContext.SaveChanges();
-            
-            _dbContext.Excerpts.AddRange(request.Excerpts);
-            _dbContext.Prices.AddRange(request.Prices);
-            _dbContext.Items.AddRange(request.Items);
-            _dbContext.SaveChanges();
-
-            _dbContext.Instances.AddRange(request.Instances);
-            _dbContext.ItemProductionOrders.AddRange(request.ItemProductionOrders);
-            _dbContext.InstanceOrderedEvents.AddRange(request.InstanceOrderedEvents);
-            _dbContext.InstanceReservedEvents.AddRange(request.InstanceReservedEvents);
-            _dbContext.InstanceDiscardedEvents.AddRange(request.InstanceDiscardedEvents);
+            DF.FakeDatabase(_dbContext);
 
             await _dbContext.SaveChangesAsync();
 
-            return HandlerResult<Unit>.Ok(Unit.New);
+            var result = new DataSeedDto
+            {
+                Companies = _dbContext.Companies.AsNoTracking().ToArray(),
+                AppUsers = _dbContext.AppUsers.AsNoTracking().ToArray(),
+                Categories = _dbContext.Categories.AsNoTracking().ToArray(),
+                UnitsOfMeasure = _dbContext.UnitsOfMeasure.AsNoTracking().ToArray(),
+                Excerpts = _dbContext.Excerpts.AsNoTracking().ToArray(),
+                Prices = _dbContext.Prices.AsNoTracking().ToArray(),
+                Items = _dbContext.Items.AsNoTracking().ToArray(),
+                ItemProductionOrders = _dbContext.ItemProductionOrders.AsNoTracking().ToArray(),
+                Instances = _dbContext.Instances.AsNoTracking().ToArray(),
+                InstanceOrderedEvents = _dbContext.InstanceOrderedEvents.AsNoTracking().ToArray(),
+                InstanceReservedEvents = _dbContext.InstanceReservedEvents.AsNoTracking().ToArray(),
+                InstanceDiscardedEvents = _dbContext.InstanceDiscardedEvents.AsNoTracking().ToArray()
+            };
+
+            return HandlerResult<DataSeedDto>.Ok(result);
         }
 
-        public ValidationResult Validate(DataSeedDto request) => new ValidationResult();
+        public ValidationResult Validate(Unit _) => new ValidationResult();
     }
 }
