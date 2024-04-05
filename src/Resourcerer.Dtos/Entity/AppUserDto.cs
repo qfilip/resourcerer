24c4803005cdce56132b056797164e3c90e24f55
qfilip <kukolj.filip@gmail.com>
2024-04-05 02:47:31 +0200

    jwt set to user dto

--- src/Resourcerer.Dtos/Entity/AppUserDto.cs
+++ src/Resourcerer.Dtos/Entity/AppUserDto.cs
@@ -3,6 +3,7 @@ public class AppUserDto : EntityDto<AppUserDto>
 {
     public string? Name { get; set; }
     public string? Password { get; set; }
+    public string? Jwt { get; set; }
     public Dictionary<string, int>? Permissions { get; set; }
     public Dictionary<string, string[]>? PermissionsMap { get; set; }
 }
