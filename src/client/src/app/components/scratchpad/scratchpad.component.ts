31bfc27e10b915ad346a7130247d1bce02a5fefa
filip.kukolj <filip.kukolj@arcadisgen.com>
2024-04-05 16:02:12 +0200

    spinner updated

--- src/client/src/app/components/scratchpad/scratchpad.component.ts
+++ src/client/src/app/components/scratchpad/scratchpad.component.ts
@@ -29,7 +29,7 @@ export class ScratchpadComponent {
     const x = setTimeout(() => {
       this.spinnerService.hide();
       clearTimeout(x);
-    }, 1000)
+    }, 5000)
   }
 
   sendPopup(t: PopupType) {
