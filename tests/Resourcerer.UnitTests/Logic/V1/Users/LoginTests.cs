faca9a47d4a9c03d9711b7556c695f63e2f88bc3
qfilip <kukolj.filip@gmail.com>
2024-04-06 13:57:22 +0200

    login testing

---
+++ tests/Resourcerer.UnitTests/Logic/V1/Users/LoginTests.cs
@@ -0,0 +1,42 @@
+﻿using Resourcerer.DataAccess.Entities;
+using Resourcerer.DataAccess.Utilities.Faking;
+using Resourcerer.Dtos.Entity;
+using Resourcerer.Logic;
+using Resourcerer.Logic.V1.Users;
+using Resourcerer.UnitTests.Utilities;
+using Resourcerer.Utilities.Cryptography;
+
+namespace Resourcerer.UnitTests.Logic.V1.Users;
+
+public class LoginTests : TestsBase
+{
+    private readonly Login.Handler _sut;
+    public LoginTests() => _sut = new(_ctx, new());
+
+    [Fact]
+    public void HappyPath__Ok()
+    {
+        // arrange
+        var username = "village_person";
+        var password = "y.m.c.a";
+        
+        DF.Fake<AppUser>(_ctx, x =>
+        {
+            x.Name = username;
+            x.PasswordHash = Hasher.GetSha256Hash(password);
+        });
+
+        var request = new AppUserDto { Name = username, Password = password };
+        _ctx.SaveChanges();
+
+        // act
+        var result = _sut.Handle(request).Await();
+
+        // assert
+        Assert.Multiple(
+            () => Assert.Equal(eHandlerResultStatus.Ok, result.Status),
+            () => Assert.NotNull(result.Object),
+            () => Assert.NotNull(result.Object!.Name)
+        );
+    }
+}
