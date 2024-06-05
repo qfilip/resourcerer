219e21f5e70533dfe3d55441356beca772846047
filip.kukolj <filip.kukolj@arcadisgen.com>
2024-06-05 15:21:54 +0200

    subpage navigation via router outlet

--- src/client/src/app/app.routes.ts
+++ src/client/src/app/app.routes.ts
@@ -1,9 +1,26 @@
 import { Routes } from '@angular/router';
 import { LoginComponent } from './pages/user/login/login.component';
 import { HomeComponent } from './components/home/home.component';
+import { CompanyOverviewPage } from './pages/company/overview/overview.page';
+import { UomOverviewPage } from './pages/unitOfMeasure/uom-overview/uom-overview.page';
+import { ItemsOverviewPage } from './pages/item/items-overview/items-overview.page';
+import { UserManagementPage } from './pages/user/user-management/user-management.page';
+import { AccountPage } from './pages/user/account/account.page';
 
 export const routes: Routes = [
     { path: '', component: HomeComponent },
-    { path: 'home', component: HomeComponent },
-    { path: 'login', component: LoginComponent }
+    { path: 'login', component: LoginComponent },
+    {
+        path: 'home',
+        component: HomeComponent,
+        children: [
+            { path: 'company', component: CompanyOverviewPage },
+            // { path: 'companies', component: CompanyOverviewPage },
+            { path: 'unitsofmeasure', component: UomOverviewPage },
+            { path: 'items', component: ItemsOverviewPage },
+            // { path: 'events', component: CompanyOverviewPage },
+            { path: 'users', component: UserManagementPage },
+            { path: 'account', component: AccountPage },
+        ]
+    },
 ];
--- src/client/src/app/components/home/home.component.ts
+++ src/client/src/app/components/home/home.component.ts
@@ -1,30 +1,25 @@
 import { Component, signal } from '@angular/core';
 import { HomepageNav } from '../../models/components/HomepageNav';
-import { AccountPage } from "../../pages/user/account/account.page";
-import { UserListComponent } from "../../pages/user/components/user-list/user-list.component";
 import { CommonModule } from '@angular/common';
-import { CompanyOverviewPage } from "../../pages/company/overview/overview.page";
-import { UserManagementPage } from "../../pages/user/user-management/user-management.page";
-import { UomOverviewPage } from "../../pages/unitOfMeasure/uom-overview/uom-overview.page";
-import { ItemsOverviewPage } from '../../pages/item/items-overview/items-overview.page';
+import { RouterLink, RouterOutlet } from '@angular/router';
 
 @Component({
     selector: 'app-home',
     standalone: true,
     templateUrl: './home.component.html',
     styleUrl: './home.component.css',
-    imports: [CommonModule, ItemsOverviewPage, AccountPage, UserListComponent, CompanyOverviewPage, UserManagementPage, UomOverviewPage]
+    imports: [CommonModule, RouterLink, RouterOutlet]
 })
 export class HomeComponent {
   page$ = signal<HomepageNav>('Company');
 
-  iconList: { pageName: HomepageNav, icon: string }[] = [
-    { pageName: 'Company', icon: 'ra ra-castle-emblem' },
-    { pageName: 'Companies', icon: 'ra ra-hydra' },
-    { pageName: 'Units of Measure', icon: 'ra ra-suits' },
-    { pageName: 'Items', icon: 'ra ra-bone-knife' },
-    { pageName: 'Events', icon: 'ra ra-lightning' },
-    { pageName: 'Users', icon: 'ra ra-queen-crown' },
-    { pageName: 'Account', icon: 'ra ra-aware' }
+  pages: { pageName: HomepageNav, route: string, icon: string }[] = [
+    { pageName: 'Company', route: 'company', icon: 'ra ra-castle-emblem' },
+    // { pageName: 'Companies', route: 'companies', icon: 'ra ra-hydra' },
+    { pageName: 'Units of Measure', route: 'unitsofmeasure', icon: 'ra ra-suits' },
+    { pageName: 'Items', route: 'items', icon: 'ra ra-bone-knife' },
+    // { pageName: 'Events', route: 'events', icon: 'ra ra-lightning' },
+    { pageName: 'Users', route: 'users', icon: 'ra ra-queen-crown' },
+    { pageName: 'Account', route: 'account', icon: 'ra ra-aware' }
   ];
 }
