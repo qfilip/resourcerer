9fb0cc8893da5468cb389237f869c569efe5dd7a
qfilip <kukolj.filip@gmail.com>
2024-06-14 21:14:47 +0200

    setting correct active page via router

--- src/client/src/app/components/home/home.component.ts
+++ src/client/src/app/components/home/home.component.ts
@@ -1,16 +1,48 @@
-import { Component, signal } from '@angular/core';
+import { Component, OnDestroy, OnInit, inject, signal } from '@angular/core';
 import { HomepageNav } from '../../models/components/HomepageNav';
 import { CommonModule } from '@angular/common';
-import { RouterLink, RouterOutlet } from '@angular/router';
+import { ActivatedRoute, NavigationEnd, Router, RouterLink, RouterOutlet } from '@angular/router';
+import { Observable, Subject, filter, map, takeUntil } from 'rxjs';
 
 @Component({
-    selector: 'app-home',
-    standalone: true,
-    templateUrl: './home.component.html',
-    styleUrl: './home.component.css',
-    imports: [CommonModule, RouterLink, RouterOutlet]
+  selector: 'app-home',
+  standalone: true,
+  templateUrl: './home.component.html',
+  styleUrl: './home.component.css',
+  imports: [CommonModule, RouterLink, RouterOutlet]
 })
-export class HomeComponent {
+export class HomeComponent implements OnInit, OnDestroy {
+
+  private router = inject(Router);
+  private subbed = new Subject();
+
+  ngOnInit(): void {
+    this.router.events.pipe(
+      takeUntil(this.subbed),
+      filter(event => event instanceof NavigationEnd),
+      map((event) => event as NavigationEnd)
+    ).subscribe({
+        next: (e) => {
+          const i = e.url.split('/');
+          const last = i[i.length - 1];
+          const route = this.pages.filter(x => x.route === last);
+          
+          if(route.length === 1) {
+            this.page$.set(route[0].pageName);
+          }
+          else {
+            this.page$.set('Company');
+          }
+        }
+      });
+  }
+
+  ngOnDestroy(): void {
+    this.subbed.next(0);
+    this.subbed.complete();
+  }
+
+
   page$ = signal<HomepageNav>('Company');
 
   pages: { pageName: HomepageNav, route: string, icon: string }[] = [
