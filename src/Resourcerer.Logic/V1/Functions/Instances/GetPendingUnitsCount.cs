3058ebd053396a62a221f5a1c5ca0053d8ffe819
qfilip <kukolj.filip@gmail.com>
2024-09-11 19:58:52 +0200

    added function to count pending instances

---
+++ src/Resourcerer.Logic/V1/Functions/Instances/GetPendingUnitsCount.cs
@@ -0,0 +1,43 @@
+﻿using Resourcerer.DataAccess.Entities;
+
+namespace Resourcerer.Logic.V1.Functions.Instances;
+
+public static partial class Instances
+{
+    /// <summary>
+    /// Get ordered instances that aren't delivered yet.
+    /// </summary>
+    /// <param name="instances">Instances of a company that created the orders.</param>
+    public static double GetPendingUnits(IEnumerable<Instance> instances)
+    {   
+        var incorrectCount = instances.Aggregate((0, 0), (acc, x) =>
+        {
+            if (x.SourceInstanceId == null)
+                acc.Item1++;
+
+            if (x.SourceInstance == null)
+                acc.Item2++;
+
+            return acc;
+        });
+
+        if (incorrectCount.Item1 > 0)
+        {
+            throw new InvalidOperationException($"No orders are available for produced instance.");
+        }
+
+        if (incorrectCount.Item2 > 0)
+        {
+            throw new InvalidOperationException($"Not all source instances are loaded.");
+        }
+
+        return instances
+            .Sum(i =>
+                i.SourceInstance!.OrderedEvents
+                    .Where(x =>
+                        x.DerivedInstanceId == i.Id &&
+                        x.CancelledEvent == null &&
+                        x.DeliveredEvent != null)
+                    .Sum(x => x.Quantity));
+    }
+}
