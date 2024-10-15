a20ebfad3e759fd92c2ba330196bc406fec4a9d3
filip.kukolj <filip.kukolj@arcadisgen.com>
2024-10-15 23:10:52 +0200

    optimized query for UnitOfMeasure deletion

--- src/Resourcerer.Logic/Utilities/Query/UnitsOfMeasure.cs
+++ src/Resourcerer.Logic/Utilities/Query/UnitsOfMeasure.cs
@@ -6,6 +6,15 @@ namespace Resourcerer.Logic.Utilities.Query;
 
 internal class UnitsOfMeasure
 {
+    public static Expression<Func<UnitOfMeasure, UnitOfMeasure>> DefaultProjection =
+        EntityBases.Expand<UnitOfMeasure>(x => new UnitOfMeasure
+        {
+            Name = x.Name,
+            Symbol = x.Symbol,
+
+            CompanyId = x.CompanyId
+        });
+
     public static Expression<Func<UnitOfMeasure, UnitOfMeasureDto>> DefaultDtoProjection =
         EntityBases.Expand<UnitOfMeasure, UnitOfMeasureDto>(x => new UnitOfMeasureDto
         {
@@ -14,4 +23,9 @@ internal class UnitsOfMeasure
 
             CompanyId = x.CompanyId
         });
+
+    public static Expression<Func<UnitOfMeasure, UnitOfMeasure>> Expand(Expression<Func<UnitOfMeasure, UnitOfMeasure>> projectionLayer)
+    {
+        return ExpressionUtils.Combine(DefaultProjection, projectionLayer);
+    }
 }
--- src/Resourcerer.Logic/V1/UnitsOfMeasure/DeleteUnitOfMeasure.cs
+++ src/Resourcerer.Logic/V1/UnitsOfMeasure/DeleteUnitOfMeasure.cs
@@ -3,6 +3,7 @@ using Microsoft.EntityFrameworkCore;
 using Resourcerer.Application.Abstractions.Handlers;
 using Resourcerer.Application.Models;
 using Resourcerer.DataAccess.Contexts;
+using Resourcerer.DataAccess.Entities;
 using Resourcerer.DataAccess.Enums;
 using Resourcerer.Dtos.Entity;
 using Resourcerer.Logic.Utilities;
@@ -23,7 +24,13 @@ public static class DeleteUnitOfMeasure
         public async Task<HandlerResult<UnitOfMeasureDto>> Handle(Guid request)
         {
             var entity = await _dbContext.UnitsOfMeasure
-                .Include(x => x.Items)
+                .AsNoTracking()
+                .Select(Utilities.Query.EntityBases.Expand<UnitOfMeasure>(x => new UnitOfMeasure
+                {
+                    Items = x.Items
+                        .Select(i => new Item { Id = i.Id })
+                        .ToList()
+                }))
                 .FirstOrDefaultAsync(x => x.Id == request);
 
             if (entity == null)
@@ -33,7 +40,8 @@ public static class DeleteUnitOfMeasure
                 return HandlerResult<UnitOfMeasureDto>.Rejected("Some items still exist associated with this unit of measure");
 
             entity.EntityStatus = eEntityStatus.Deleted;
-
+            _dbContext.Entry(entity).Property(x => x.EntityStatus).IsModified = true;
+            
             await _dbContext.SaveChangesAsync();
 
             return HandlerResult<UnitOfMeasureDto>.Ok(Mapper.Map(entity));
