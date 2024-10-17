ec288342c319af71d8394258c4e41d00773ff2db
filip.kukolj <filip.kukolj@arcadisgen.com>
2024-10-17 18:10:15 +0200

    frontend work

--- src/client/src/app/pages/browse-item/browse-item/browse-items.page.ts
+++ src/client/src/app/pages/browse-item/browse-item/browse-items.page.ts
@@ -1,11 +1,14 @@
 import { Component, inject, OnInit } from '@angular/core';
 import { ItemController } from '../../../controllers/item.controller';
 import { UserService } from '../../../services/user.service';
+import { Observable } from 'rxjs';
+import { IV1ItemShoppingDetails } from '../../../models/dtos/interfaces';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'browse-items',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './browse-items.page.html',
   styleUrl: './browse-items.page.css'
 })
@@ -13,10 +16,11 @@ export class BrowseItemsPage implements OnInit {
 
   private userService = inject(UserService)
   private itemController = inject(ItemController)
+  itemShoppingDetails$!: Observable<IV1ItemShoppingDetails[]>;
   ngOnInit(): void {
     const user = this.userService.user();
-    this.itemController.getItemsShoppingDetails(user!.company.id)
-      .subscribe(xs => console.log(xs));
+    this.itemShoppingDetails$ =  this.itemController
+      .getItemsShoppingDetails(user!.company.id);
   }
 
 }
