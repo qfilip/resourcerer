df590cf77414e5eed3159e8b4959f6f5c65ace66
qfilip <kukolj.filip@gmail.com>
2024-11-25 19:27:51 +0100

    Item functions

---
+++ src/Resourcerer.Logic/V1/Functions/Items/UsedInComposites.cs
@@ -0,0 +1,12 @@
+﻿using Resourcerer.DataAccess.Entities;
+
+namespace Resourcerer.Logic.V1.Functions;
+
+public static partial class Items
+{
+    public static IEnumerable<Item> UsedInComposites(Item item)
+    {
+        return item.ElementRecipeExcerpts
+            .Select(x => x.Recipe!.CompositeItem!);
+    }
+}
