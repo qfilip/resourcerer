4270fa0e66f1f6fde037c5bd3edf515a15f657f1
qfilip <kukolj.filip@gmail.com>
2025-04-20 11:20:38 +0200

    excerpt dialog

---
+++ src/client/src/app/features/item/components/excerpt-dialog/excerpt-dialog.component.ts
@@ -0,0 +1,54 @@
+import { Component, output, signal, ViewChild } from '@angular/core';
+import { DialogWrapperComponent } from '../../../../shared/features/common-ui/components/dialog-wrapper/dialog-wrapper.component';
+import { IItemDto } from '../../../../shared/dtos/interfaces';
+
+@Component({
+  selector: 'app-excerpt-dialog',
+  imports: [DialogWrapperComponent],
+  templateUrl: './excerpt-dialog.component.html',
+  styleUrl: './excerpt-dialog.component.css'
+})
+export class ExcerptDialogComponent {
+  @ViewChild('wrapper') private wrapper!: DialogWrapperComponent;
+  onOk = output<{ item: IItemDto, qty: number }[]>();
+  private _$items = signal<IItemDto[]>([]);
+  $items = this._$items.asReadonly();
+
+  private _$recipe = signal<{ item: IItemDto, qty: number }[]>([]);
+  $recipe = this._$recipe.asReadonly();
+
+  open(items: IItemDto[]) {
+    this._$items.set(items);
+    this.wrapper.open();
+  }
+
+  addItem(item: IItemDto) {
+    this._$items.update(xs => xs.filter(x => x.id !== item.id));
+    this._$recipe.update(xs => xs.concat({ item: item, qty: 0 }))
+  }
+
+  updateRecipe(item: IItemDto, qty: number | string) {
+    this._$recipe.update(xs =>
+      xs.reduce((acc, x) => {
+        if(x.item.id === item.id)
+          x.qty = qty as number;
+
+        return acc.concat(x)
+      }, [] as {item: IItemDto, qty: number }[])
+    )
+  }
+
+  remove(item: IItemDto) {
+    this._$items.update(xs => xs.concat(item));
+    this._$recipe.update(xs => xs.filter(x => x.item.id !== item.id));
+  }
+
+  create() {
+    this.onOk.emit(this._$recipe());
+    this.wrapper.close();
+  }
+
+  close() {
+    this.wrapper.close();
+  }
+}
